6ca3f4778eb68a90b9dce54355da5d5e491d6b76
doanvu20122000 <doanvu20122000@gmail.com>
2025-04-08 18:16:07 +0700

    Implement Onboarding Screen
    - Create `OnboardingScreen` with basic text layout.

--- app/src/main/java/com/jin/coinpay/presentation/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/jin/coinpay/presentation/ui/onboarding/OnboardingScreen.kt
@@ -1,8 +1,10 @@
 package com.jin.coinpay.presentation.ui.onboarding
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun OnboardingScreen(navController: NavController) {
+    Text("Onboarding Screen")
 }
